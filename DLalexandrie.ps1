8da9c33bf5dfa1b174ca4faaeba064304ed2ecf2
ANNUAIRE\frup52475 <thierrybuisson@live.fr>
2017-06-07 00:25:24 +0200

    no message

--- DLalexandrie.ps1
+++ DLalexandrie.ps1
@@ -1,16 +1,20 @@
 
 # https://stackoverflow.com/questions/16575419/powershell-retrieve-json-object-by-field-value
 $jsonConfigFile = "alexandrie.json"
-
-$JSON = Get-Content $jsonConfigFile | Out-String | ConvertFrom-Json
+$jsoncontent = Get-Content $jsonConfigFile | Out-String | Replace('\"','"')
+$JSON = $jsoncontent  | ConvertFrom-Json
 foreach( $book in $JSON.entities ) {
     $bcontent = $book.E 
-    foreach ($c in $bcontent){
-$c
-        $t = $c.DN
-        $t
 
-    }
+   $i = $bcontent[0]
+   $i
+   write-host ""
+    # foreach ($c in $bcontent){
+    #     $c.DN
+    #     $t = $c  | ConvertFrom-Json
+    #     # $t
+
+    # }
     # write-host $bcontent
     
     # $bcontent.DN 
